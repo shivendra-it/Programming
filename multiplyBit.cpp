5eeeca78ebc79fcf454428eae616d5174c9e2a54
shivendra-pratap <shivendraps15@gmail.com>
2017-04-04 10:02:21 +0530

    multiply using bits

--- multiplyBit.cpp
+++ multiplyBit.cpp
@@ -2,31 +2,29 @@
 using namespace std;
 
 long sum(long a,long b){
-long su = 0, carryin = 0, k = 1, tempA = a, tempB = b;
-while(tempA != 0 || tempB != 0) {
-long ak = a & k, bk = b & k ;
-long carryout = (ak & bk) | (ak & carryin)|(bk & carryin);
-su |= (ak ^ bk ^ carryin);
-carryin = carryout << 1;
-k <<= 1 ;
-tempA >>= 1 ;
-tempB >>= 1 ;
-}
-return su|carryin;
+    long su = 0, carryin = 0, k = 1, tempA = a, tempB = b;
+    while(tempA != 0 || tempB != 0) {
+        long ak = a & k, bk = b & k ;
+        long carryout = (ak & bk) | (ak & carryin)|(bk & carryin);
+        su |= (ak ^ bk ^ carryin);
+        carryin = carryout << 1;
+        k <<= 1 ;
+        tempA >>= 1 ;
+        tempB >>= 1 ;
+    }
+    return su|carryin;
 }
 
-long sum1(long a,long b){
-    return a+b;
-}
+
 
 long mul(long a,long b){
     long s =0;
     while(a != 0){
-        if((a & 1)!= 0){
-        s += sum1(s,b); 
-        }
-        a = a >> 1;
-        b = b << 1;
+        if(a&1)
+        s = sum(s,b); 
+        a >>= 1;
+        b <<= 1;
+        cout<<"s = "<<s<<" a = "<<a<<" b = "<<b<<endl;
     }
 return s;
 }
