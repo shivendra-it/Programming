70976e3c7f20ba915380ff02493ac8cb77c385d9
shivendra-pratap <shivendraps15@gmail.com>
2017-05-05 10:13:15 +0530

    codes

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[1000];
+void fun(int arr[],int n,int k){
+fill(arr,arr+2*n,-1);
+arr[0]=1;
+for(int i = 1;i<n;i++){
+    if(arr[i-1] == a[2*i +1])
+        cout
+}
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int k = 1;
+    fun(arr,n,k)
+    return 0;
+}
