7dee43bb1dd40aaf12c6dcf8d31838da734379c7
Shivendra Pratap Singh <shivendraps15@gmail.com>
2017-07-06 18:34:15 +0530

    changes in codes

---
+++ DistinctSubsequences.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class DistinctSubsequences { 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            String in = sc.next();
+            System.out.println(in);
+            char[] inp = in.toCharArray();
+        }
+    }
+}
